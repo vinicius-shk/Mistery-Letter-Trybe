7a1e3915ef98fdf6b35fad16c60503f9c97a4ce2
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-05-01 10:41:59 -0300

    Conclusão do requisito 4

--- script.js
+++ script.js
@@ -3,8 +3,28 @@ let cartaUser = document.getElementById('carta-texto');
 let areaCarta = document.getElementById('carta-gerada');
 btnCriar.addEventListener('click', geraCarta);
 function geraCarta() {
-  let inputUser = cartaUser.value;
-  let novaCarta = document.createElement('span');
-  novaCarta.innerHTML = inputUser;
-  areaCarta.appendChild(novaCarta);
+  let filhos = areaCarta.childNodes;
+  if (filhos.length !== 0) {
+  for (let key = filhos.length; key > 0; key -= 1) {
+    areaCarta.removeChild(areaCarta.firstChild);
+    }
+  }
+  let texto = cartaUser.value;
+  let palavra = '';
+  let arrayPalavra = [];
+  for (let key = 0; key < texto.length; key += 1) {
+    if (texto[key] !== ' ') {
+      palavra += texto[key];
+      console.log(palavra);
+    } else {
+      arrayPalavra.push(palavra);
+      palavra = '';
+    }
+  }
+  arrayPalavra.push(palavra);
+  for (let key in arrayPalavra) {
+    let novaCarta = document.createElement('span');
+    novaCarta.innerHTML = arrayPalavra[key];
+    areaCarta.appendChild(novaCarta);
+  }
 }
