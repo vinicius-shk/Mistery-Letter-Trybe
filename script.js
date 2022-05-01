c1d9e4aea239178ed14ba3d00a533f6203070610
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-05-01 11:21:54 -0300

    Conclusão do requisito 16

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let btnCriar = document.getElementById('criar-carta');
 let cartaUser = document.getElementById('carta-texto');
 let areaCarta = document.getElementById('carta-gerada');
+let grpEstilo = ['newspaper', 'magazine1', 'magazine2'];
+let grpTamanho = ['medium', 'big', 'reallybig'];
+let grpRotacao = ['rotateleft', 'rotateright'];
+let grpInclinacao = ['skewleft', 'skewright'];
 btnCriar.addEventListener('click', geraCarta);
 function geraCarta() {
   let filhos = areaCarta.childNodes;
@@ -27,6 +31,10 @@ function geraCarta() {
       arrayPalavra.push(palavra);
       for (let key in arrayPalavra) {
         let novaCarta = document.createElement('span');
+        novaCarta.classList.add(grpTamanho[Math.floor(Math.random()* 3)]);
+        novaCarta.classList.add(grpEstilo[Math.floor(Math.random()* 3)]);
+        novaCarta.classList.add(grpRotacao[Math.floor(Math.random()* 2)]);
+        novaCarta.classList.add(grpInclinacao[Math.floor(Math.random()* 2)]);
         novaCarta.innerHTML = arrayPalavra[key];
         areaCarta.appendChild(novaCarta);
       }
